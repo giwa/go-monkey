9c84cede584ed38f4b6eb9bb2e2ac58d5f8e58b9
giwa <ugw.gi.world@gmail.com>
2018-09-08 15:26:52 +0900

    support repl in 1.5

---
+++ src/monkey/repl/repl.go
@@ -0,0 +1,30 @@
+package repl
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"monkey/lexer"
+	"monkey/token"
+)
+
+const PROMPT = ">> "
+
+func Start(ln io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(ln)
+
+	for {
+		fmt.Printf(PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := scanner.Text()
+		l := lexer.New(line)
+
+		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
+			fmt.Printf("%+v\n", tok)
+		}
+	}
+}
